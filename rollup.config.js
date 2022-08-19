4ad55fdef77c0f50d464261732ddfed14f76f551
Hubert Skuza <skuza.hs@pg.com>
2022-08-19 15:02:26 +0200

    configuration for css files

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import sass from 'rollup-plugin-sass'
 import { terser } from 'rollup-plugin-terser'
 
 const packageJson = require('./package.json')
@@ -23,6 +25,8 @@ export default [
       }
     ],
     plugins: [
+      sass(),
+      postcss(),
       peerDepsExternal(),
       resolve(),
       commonjs(),
